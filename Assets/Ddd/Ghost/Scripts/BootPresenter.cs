d3623a991c23b5b980e9b20509cb1d1432f30ff6
StickeyFingersESW9 <koiwai.1059373.me03sf.ilohas@gmail.com>
2018-04-30 11:43:03 +0900

    namespaceの追加と整形

--- Assets/Ddd/Ghost/Scripts/BootPresenter.cs
+++ Assets/Ddd/Ghost/Scripts/BootPresenter.cs
@@ -2,15 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BootPresenter : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+namespace Ddd.Ghost
+{
+	public class BootPresenter : MonoBehaviour
+	{
+		// Use this for initialization
+		void Start()
+		{
+		}
 		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+		// Update is called once per frame
+		void Update()
+		{
+		}
 	}
 }
